206a8e3af1b6a59ba0e32ebe1619fb2fa4edfcb8
gukurhgodp31 <wushangza19931030@126.com>
2021-03-30 11:09:07 +0800

    db conn

--- CSharpHW/CSharpHW/DBUtility.cs
+++ CSharpHW/CSharpHW/DBUtility.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace CSharpHW
 {
     class DBUtility
     {
+        private MySqlConnection conn;
+        public void connect(String password, String dbName)
+        {
+            string connection = "server=localhost;user id=root;password="+password+";database="+dbName+"; pooling=true;";
+            conn = new MySqlConnection(connection);
+            conn.Open();
+        }
+
+        public void disconnect()
+        {
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
     }
 }
